3213005eeaa3508ac7db0d07bdcf111732baac87
Catherine Xie <catherinexie2014@gmail.com>
2025-09-04 18:43:43 -0400

    feat: random cocktail logic with thumbnail

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,18 +1,44 @@
 // import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Dashboard() {
+  const [randomThumb, setRandomThumb] = useState("");
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:8080/api");
     console.log("response:", response.data);
   };
 
+  const getRandom = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+      );
+      const drink = response.data.drinks[0];
+      console.log("random:", drink);
+      setRandomThumb(`${drink.strDrinkThumb}/medium`);
+    } catch (error) {
+      console.error("Error fetching random cocktail:", error);
+    }
+  };
+
   useEffect(() => {
-    fetchAPI();
+    // fetchAPI();
+    // getRandom();
   }, []);
 
-  return <p>hello this is dashboard</p>;
+  return (
+    <>
+      <p>hello this is dashboard</p>
+      <p onClick={getRandom}>surprise me</p>
+
+      {randomThumb && (
+        <div>
+          <img src={randomThumb} alt="Random cocktail" />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Dashboard;
