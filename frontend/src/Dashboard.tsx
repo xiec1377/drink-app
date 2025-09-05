7762a84003e4919e3a931da9419da4a383efc488
Catherine Xie <catherinexie2014@gmail.com>
2025-09-05 12:02:10 -0400

    feat: simple search bar feature

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,9 +1,26 @@
 // import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import DrinkCard from "./components/DrinkCard";
+
+type Cocktail = {
+  strDrink: string;
+  strDrinkThumb: string;
+  strAlcoholic: string;
+  strTags: string;
+};
 
 function Dashboard() {
   const [randomThumb, setRandomThumb] = useState("");
+  const [cocktailFields, setCocktailFields] = useState<Cocktail | null>(null);
+  const [search, setSearch] = useState("");
+  const [searchList, setSearchList] = useState<Cocktail[]>([]);
+
+  const handleInputChange = (e) => {
+    console.log("e:", e.target.value);
+    setSearch(e.target.value);
+  };
+
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:8080/api");
     console.log("response:", response.data);
@@ -15,8 +32,25 @@ function Dashboard() {
         "https://www.thecocktaildb.com/api/json/v1/1/random.php"
       );
       const drink = response.data.drinks[0];
+      setCocktailFields(response.data.drinks[0]);
       console.log("random:", drink);
-      setRandomThumb(`${drink.strDrinkThumb}/medium`);
+      setRandomThumb(`${drink.strDrinkThumb}/small`);
+    } catch (error) {
+      console.error("Error fetching random cocktail:", error);
+    }
+  };
+
+  const searchCocktail = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`
+      );
+      if (response.data.drinks) {
+        console.log("search results:", response.data.drinks);
+        setSearchList(response.data.drinks);
+      } else {
+        console.log("No cocktails found for:", search);
+      }
     } catch (error) {
       console.error("Error fetching random cocktail:", error);
     }
@@ -30,13 +64,39 @@ function Dashboard() {
   return (
     <>
       <p>hello this is dashboard</p>
-      <p onClick={getRandom}>surprise me</p>
-
-      {randomThumb && (
-        <div>
-          <img src={randomThumb} alt="Random cocktail" />
-        </div>
-      )}
+      <button onClick={getRandom}>surprise me</button>
+      {cocktailFields?.strDrinkThumb && (
+        <DrinkCard
+          name={cocktailFields?.strDrink ?? ""}
+          image={`${cocktailFields?.strDrinkThumb}/small`}
+          alcoholic={cocktailFields?.strAlcoholic ?? ""}
+          //   tags={cocktailFields?.strTags ?? ""}
+        />
+      )}{" "}
+      <div className="flex items-center border border-gray-300 rounded-lg overflow-hidden">
+        <input
+          type="text"
+          placeholder="search for cocktail"
+          className="flex-grow p-2 focus:outline-none"
+          value={search}
+          onChange={handleInputChange}
+        />
+        <button
+          className="bg-blue-500 text-white p-2 px-4 hover:bg-blue-600 focus:outline-none"
+          onClick={searchCocktail}
+        >
+          Search
+        </button>
+      </div>
+      {searchList &&
+        searchList.map((cocktail) => (
+          <DrinkCard
+            name={cocktail?.strDrink ?? ""}
+            image={`${cocktail?.strDrinkThumb}/small`}
+            alcoholic={cocktail?.strAlcoholic ?? ""}
+            //   tags={cocktailFields?.strTags ?? ""}
+          />
+        ))}{" "}
     </>
   );
 }
---
+++ frontend/src/components/DrinkCard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function DrinkCard({
+  name,
+  image,
+  alcoholic,
+  tags,
+  onClick,
+  children,
+}: {
+  name?: string;
+  image?: string;
+  alcoholic?: string;
+  tags?: string;
+  children?: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col">
+      <img src={image} />
+      <p>{name}</p>
+      <p>{alcoholic}</p>
+      {tags?.split(",").map((tag) => (
+        <p key={tag}>{tag}</p>
+      ))}
+    </div>
+  );
+}
+
+export default DrinkCard;
